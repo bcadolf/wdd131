97001d83b71d2d1d8afb7e32929dd785f5cad125
bcadolf <adolfbrandon96@gmail.com>
2024-07-12 23:46:06 -0400

    week 3 submit

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,23 @@ const today = new Date();
 
 currentyear.innerHTML = "&copy" + today.getFullYear();
 lastModified.innerHTML = "Last Modified: " + document.lastModified;
+
+// Wind Chill with Fairenhieght Formula
+function calculateWindChill(tempFahrenheit, windSpeedMph) {
+    const windChill = 35.74 + 0.6215 * tempFahrenheit - 35.75 * Math.pow(windSpeedMph, 0.16) + 0.4275 * tempFahrenheit * Math.pow(windSpeedMph, 0.16);
+    return windChill;
+}
+
+// Use the function to add to the html
+const temperature = 75; // Fahrenheit
+const windSpeed = 4; // mph
+const result = Math.round(calculateWindChill(temperature, windSpeed));
+const windChill = document.querySelector("#windChill");
+
+if (temperature <= 50 && windSpeed > 3) {
+    chill = result - temperature + " °F";
+} else {
+    chill = "N/A";
+}
+
+windChill.innerHTML = chill
