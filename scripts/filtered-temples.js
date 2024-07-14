740a7221d9314e6541895470c662fd21c3742b5a
bcadolf <adolfbrandon96@gmail.com>
2024-07-14 12:57:55 -0400

    week4 submission

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,120 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+currentyear.innerHTML = "&copy" + today.getFullYear();
+lastModified.textContent = "Last Modified: " + document.lastModified;
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    // Add more temple objects here...
+];
+
+function templeCard(temples) {
+    const table = document.createElement("table")
+    table.innerHTML = `<tr>
+        <th colspan="2">${temples.templeName}</th></tr>
+        <tr>
+            <th>Location: </th>
+            <td>${temples.location}<td>
+        </tr>
+        <tr>
+            <th>Dedicated: </th>
+            <td>${temples.dedicated}</td>
+        </tr>
+        <tr>
+            <th>Size: </th>
+            <td>${temples.area} sq ft</td>
+        </tr>
+        <tr>
+            <td colspan="2"><img src="${temples.imageUrl}" alt="${temples.templeName}" loading="lazy"></td>
+        `
+    return table
+
+};
+
+function loadTempleCardples(filter) {
+    const div = document.querySelector(".img-grid");
+    div.innerHTML = "";
+
+    temples
+        .filter(filter)
+        .forEach(temple => {
+            const createTempleCard = templeCard(temple);
+            div.appendChild(createTempleCard);
+        });
+}
+
+document.getElementById("home").addEventListener("click", () => loadTempleCardples(() => true));
+document.getElementById("old").addEventListener("click", () => loadTempleCardples(temple => parseInt(temple.dedicated.slice(0, 4))) < 1900);
+document.getElementById("new").addEventListener("click", () => loadTempleCardples(temple => parseInt(temple.dedicated.slice(0, 4))) > 2000);
+document.getElementById("large").addEventListener("click", () => loadTempleCardples(temple => temple.area > 90000));
+document.getElementById("small").addEventListener("click", () => loadTempleCardples(temple => temple.area < 10000));
+
+loadTempleCardples(() => true);
+
