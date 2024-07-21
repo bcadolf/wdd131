8f654857c76e253fc67171fbde6738a983752f91
bcadolf <adolfbrandon96@gmail.com>
2024-07-21 16:34:14 -0400

    homoe page finished

--- itbeginswithwood/scripts/main.js
+++ itbeginswithwood/scripts/main.js
@@ -5,3 +5,32 @@ const today = new Date();
 
 currentyear.innerHTML = "&copy" + today.getFullYear();
 lastModified.innerHTML = "Last Modified: " + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const activePage = document.getElementById("activePage");
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const journal = document.getElementById("journal");
+const projects = document.getElementById("projects");
+
+activePage.textContent = localStorage.getItem("title")
+
+home.addEventListener("click", () => {
+    localStorage.setItem("title", home.textContent)
+});
+about.addEventListener("click", () => {
+    localStorage.setItem("title", about.textContent)
+});
+journal.addEventListener("click", () => {
+    localStorage.setItem("title", journal.textContent)
+});
+projects.addEventListener("click", () => {
+    localStorage.setItem("title", projects.textContent)
+});
